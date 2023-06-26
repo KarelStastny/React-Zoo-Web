cc1acbcbf84cabb11cf275f2f5e44794525a8817
Karel Šťastný <karell29@icloud.com>
2023-06-26 13:48:55 +0200

    Základní struktura vložena a rozdělení souborů

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  return <div className="app">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis beatae ducimus eum maxime tempore aliquam fuga soluta voluptates assumenda, vitae inventore? Commodi ad, quasi sapiente rerum qui dignissimos omnis voluptas.
+    Distinctio, est nesciunt voluptates alias eos veniam explicabo ratione, magnam animi, omnis qui reiciendis a iusto commodi corrupti. Id excepturi mollitia, quos impedit aut dignissimos modi rerum ipsum ab dolore.
+    Nam neque ab unde nobis iste fugit cumque ipsum ratione minus, iusto in quibusdam officiis, molestias numquam labore sunt. Voluptas itaque doloribus quos nostrum? Suscipit aperiam quisquam rem asperiores ipsa.
+    Soluta asperiores in eius nemo quibusdam fugit perferendis ullam, incidunt ipsum eos at laborum accusantium libero illo rerum dolorum, modi tempore nisi odit mollitia recusandae, fuga repudiandae quod? At, nisi.
+    Magnam corporis mollitia nostrum amet? Dicta, optio! Repudiandae eius ducimus quo accusamus earum fuga quibusdam quisquam at blanditiis natus consectetur, perferendis, amet eaque adipisci temporibus fugit animi iusto, necessitatibus atque.
+    Ex labore, inventore deleniti laudantium quibusdam autem? Accusantium perspiciatis quam nisi expedita aliquam quo fugit id blanditiis soluta, rem nobis maiores ea similique. Debitis dignissimos temporibus quasi eveniet! Nobis, ullam.
+    Quae nihil saepe voluptatibus quaerat, placeat eligendi quisquam distinctio architecto dolore labore modi vitae temporibus dolorem id, odio nemo doloribus asperiores numquam excepturi impedit qui ullam corporis aperiam. Neque, quisquam?
+    Nostrum dolor nam laborum veritatis, deleniti velit ullam repellat illum cumque, autem ea, voluptatem a nobis itaque inventore dicta! Similique, expedita commodi! Expedita numquam architecto, eos nam omnis consequuntur qui.
+    Quae inventore magni nisi fugit aliquam quidem culpa quisquam exercitationem dolorum est, molestias iste a reprehenderit aut sit rem nihil expedita assumenda architecto neque voluptatem repudiandae sed ducimus qui. Harum?
+    Voluptates provident magnam dignissimos corrupti molestiae explicabo accusantium cum quisquam. Tempora fugiat unde reprehenderit nobis laudantium, molestiae ab temporibus nisi nostrum incidunt quis eius illum omnis ratione! Quos, voluptatibus omnis!
+  </div>
 }
 
-export default App;
+export default App
+
+
+
