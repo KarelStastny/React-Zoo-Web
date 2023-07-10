525e55683a98856091b52aaa8261e18e7d84431c
Karel Šťastný <karell29@icloud.com>
2023-07-10 12:13:30 +0200

    Vytvořena stránka pro návštěvníky, kde se budou z local storage zobrazovat všechny aktualizace zadané z admin stránky

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -4,47 +4,47 @@ import "./Admin.scss"
 import ActualityForm from '../components/ActualityForm'
 
 const Admin = () => {
-    // const [userName, setUserName] = useState("")
-    // const [password, setPassword] = useState("")
-    // // přepínač pokud je autorizace správně nebo špatně
-    // const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [userName, setUserName] = useState("")
+    const [password, setPassword] = useState("")
+    // přepínač pokud je autorizace správně nebo špatně
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
 
 
-    // // Uložení jména a hesla z formuláře
-    // const writeName = (e) => {
-    //     setUserName(e.target.value)
-    // }   
-    // const writePassword = (e) => {
-    //     setPassword(e.target.value)
-    // }   
+    // Uložení jména a hesla z formuláře
+    const writeName = (e) => {
+        setUserName(e.target.value)
+    }   
+    const writePassword = (e) => {
+        setPassword(e.target.value)
+    }   
 
-    // // Přihlášení
-    // const login = (e) => {
-    //     e.preventDefault()
+    // Přihlášení
+    const login = (e) => {
+        e.preventDefault()
 
-    //     // Heslo
-    //     const adminPassword = "admin"
-    //     const adminName = "admin"
+        // Heslo
+        const adminPassword = "admin"
+        const adminName = "admin"
 
-    //     // Oveření hesla a jména
-    //     if(password === adminPassword && userName === adminName){
-    //         setIsAuthenticated(true)
-    //     }else{
-    //         alert ("Nespárvné Heslo. Jméno i Heslo jsou 'admin'" )
-    //     }
-    // }
+        // Oveření hesla a jména
+        if(password === adminPassword && userName === adminName){
+            setIsAuthenticated(true)
+        }else{
+            alert ("Nespárvné Heslo. Jméno i Heslo jsou 'admin'" )
+        }
+    }
 
-    // // Pokud není ověřena autorizace připrav formulář
+    // Pokud není ověřena autorizace připrav formulář
 
-    // if(!isAuthenticated){
-    //     // po odeslání zavolat přihlašovací funkci
-    //     return <form className='login-admin-page' onSubmit={login}>
-    //         <input type="text" placeholder='jméno "admin"' onChange={writeName} />
-    //         <input type="text" placeholder='heslo "admin"' onChange={writePassword} />
-    //         <input type="submit" />
-    //         </form>
+    if(!isAuthenticated){
+        // po odeslání zavolat přihlašovací funkci
+        return <form className='login-admin-page' onSubmit={login}>
+            <input type="text" placeholder='jméno "admin"' onChange={writeName} />
+            <input type="text" placeholder='heslo "admin"' onChange={writePassword} />
+            <input type="submit" />
+            </form>
         
-    // }
+    }
 
 
 
--- src/pages/Aktuality.js
+++ src/pages/Aktuality.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import HeddingBodyLayout from '../components/HeddingBodyLayout'
-
+import "./Aktuality.scss"
 
 const Aktuality = () => {
-  return (
-    <div>
-     
+  // VYtažení dat z Local Storage
+  const actualityData = JSON.parse(localStorage.getItem("actuality")) || [];
+
+
+  return <div className='actuality-user'>
       <HeddingBodyLayout/>
+      <div className="actuality-user-container row">
+        <h1>Aktuality </h1>
+        {
+          actualityData.map( (one, index) => {
+          return  <div key={index} className="actuality-box">
+              <p>{one.date}</p>
+              <p>{one.heading}</p>
+              <p>{one.text}</p>
+            </div>
+          })
+        }
+
+      </div>
     </div>
-  )
+  
 }
 
 export default Aktuality
+
+
