9f57b31116e1d23b7a45be78402603d2b83c92ed
Karel Šťastný <karell29@icloud.com>
2023-07-13 13:49:54 +0200

    Vytvořena komponenta která mi vypisuje podle id oblíbená zvířata

--- src/pages/Favourite.js
+++ src/pages/Favourite.js
@@ -1,15 +1,37 @@
 import React, {useState} from 'react'
 import animals from '../data/animals'
+import "./Favourite.scss"
+import { Link } from "react-router-dom"
 
 
 const Favourite = () => {
     const favorite = JSON.parse(localStorage.getItem('favorite'));
-    console.log(favorite);
+
+
 
 
   return <div className="favourite-page">
+        <h1>Vaše Oblíbená zvířata</h1>
+    <div className="favorite-container row2">
 
+    {
+    animals.map((oneA) => {
+      return  favorite.map((oneF) => {
+            if(oneA.id === oneF){
+               return <Link  to={`/zvirata/${oneA.id}`} key={oneA.id}>
+               
+                <img src={oneA.url} alt="" />
+                <p>{oneA.name}</p>
+               </Link>
+            }
+        })
 
+        
+    })
+    }
+
+    
+</div>
   </div>
 }
 
